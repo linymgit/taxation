457f26f1cf90958ddd4d84eb73405e73c0307be4
linym <18316471919@163.com>
2019-07-02 23:24:20 +0800

    project init

---
+++ src/test/java/com/haha/taxation/TaxationApplicationTests.java
@@ -0,0 +1,45 @@
+package com.haha.taxation;
+
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.metadata.Sheet;
+import com.alibaba.excel.support.ExcelTypeEnum;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaxationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void writeWithoutHead() throws IOException {
+        try (OutputStream out = new FileOutputStream("test.xlsx");) {
+            ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX, false);
+            Sheet sheet1 = new Sheet(1, 0);
+            sheet1.setSheetName("sheet1");
+            List<List<String>> data = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                List<String> item = new ArrayList<>();
+                item.add("item0" + i);
+                item.add("item1" + i);
+                item.add("item2" + i);
+                data.add(item);
+            }
+            writer.write0(data, sheet1);
+            writer.finish();
+        }
+    }
+
+
+}
